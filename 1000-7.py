ee09dcc4a0fbded0731189e3096195d28d2f0d2b
analldn <117182250+analldn@users.noreply.github.com>
2022-11-01 17:30:41 +0300

    Add files via upload

---
+++ 1000-7.py
@@ -0,0 +1,10 @@
+import time
+a = 1007
+while a <=1007:
+    a -= 7
+    print (str(a) + '- 7 = ' + str(a-7))
+    time.sleep(0.15)
+    if a == 6:
+        break
+print ("Я ГУЛЬ!АЩИХИТЕО АШИХИТЕО!!")
+
